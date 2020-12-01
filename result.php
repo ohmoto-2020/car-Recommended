04127c176e4d2b93737a179cf1ae6b1cb4d6682f
おーもと <vitamin29290209@gmail.com>
2020-12-01 22:10:37 +0900

    キャッシュファイル作成プログラム途中

--- result.php
+++ result.php
@@ -18,7 +18,7 @@ $stmh->bindValue(':style', $search_style, PDO::PARAM_STR);
 $stmh->bindValue(':size', $search_size, PDO::PARAM_STR);
 $stmh->bindValue(':country', $search_country, PDO::PARAM_STR);
 $stmh->bindValue(':uses', $search_uses, PDO::PARAM_STR);
-$stmh->execute();
+$stmh->execute();//SQL文実行
 
 
 //API
@@ -27,7 +27,8 @@ $baseurl = "http://webservice.recruit.co.jp/carsensor/usedcar/v1/";
 //リクエストパラメータ
 $key = '277fa2e16d8a6ebb'; // あなたのAPIキー
 
-
+$max = 5; //コンテンツの最大数
+$contents = array();
 $totalCars = array();
 //該当車種分繰り返す(SQL)
 while($sqlCar = $stmh->fetch(PDO::FETCH_ASSOC)){
@@ -35,11 +36,22 @@ while($sqlCar = $stmh->fetch(PDO::FETCH_ASSOC)){
   $brand = htmlspecialchars($sqlCar['maker']);//メーカー
   $model = htmlspecialchars($sqlCar['name']); //車種名
   $price = htmlspecialchars($sqlCar['price']); //価格
-  //リクエストURLを組み立て
+  //リクエストURLの組み立て
   $url = "$baseurl?key=$key&brand=$brand&model=$model&count=40";
 
-  //XMLパース
-  $apiCars = simplexml_load_file($url)->{'usedcar'};
+
+  $file_name = "./cache/" . $brand . "_" . $model . ".selial";//キャッシュファイル作成
+
+  //$file_name名のキャッシュファイルが存在するか否か
+  if(is_file($file_name)){
+    $xmlStrings = file_get_contents($file_name);//保存されたキャッシュファイルを取ってくる
+  } else {
+    $xmlStrings = file_get_contents($url);//リクエストURLの文字列を取ってくる
+  }
+  $xml = simplexml_load_string($xmlStrings);//xmlをオブジェクト化
+  $apiCars = $xml->{'usedcar'};//取ってきたオブジェクトの車情報をとる
+
+  file_put_contents($file_name,$xmlStrings);//$file_name名で中身を$xmlStringsとしてキャッシュファイルを作る
 
   $filteredCars = array();
   //該当車種の台数分繰り返す(API)
@@ -52,8 +64,8 @@ while($sqlCar = $stmh->fetch(PDO::FETCH_ASSOC)){
     }
   }
   $totalCars[] = array_slice($filteredCars,0,3);
-
 }
+
 // foreach ($totalCars as $value) {
 //   var_dump($value);
 // }
